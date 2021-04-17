cfc6a2d9ff18595f5ab1a8fe75aa20a78a799c4a
zhongren <zhrren@qq.com>
2021-04-18 07:51:09 +0800

    update

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,6 +3,6 @@ import { ApiModule } from './api.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(ApiModule);
-  await app.listen(3000);
+  await app.listen(14042);
 }
 bootstrap();
